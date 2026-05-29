ee154750cb15bdcd02dd07d4515f369e0fd6cb3f
james-automation <james.automation1@gmail.com>
2026-05-29 17:05:29 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-  if (window.location.pathname == '/account/register') {
+
+  const url= window.location.pathname;
+
+  if (url === '/account/register') {
     const form = document.getElementById('RegisterForm');
     if (form && form.children[2])
       form.children[2].style.display = "none";
@@ -7,10 +10,10 @@ $(document).ready(function() {
 
   const ul= document.createElement('ul')
 
-  ul.style.cssText= "margin:-25px auto 10px; width:100%;display: flex;justify-content: center;";
+  ul.style.cssText= "margin:-15px auto 15px; width:100%;display: flex;justify-content: center;";
   ul.innerHTML= `
-    <li><a class="btn-custom-extra" href="https://play.app.goo.gl/?link=https://play.google.com/store/apps/details?id=app.intra&amp;hl=en&amp;gl=US" rel="nofollow" margin-right:"5px" target="_blank">ANTI NAWALA</a></li>
-    <li><a class="btn-custom-extra" href="/demo/slot-demo-pgs/" font-weight:bold;">SLOT DEMO PGSOFT</a></li>
+    <li><a class="btn-custom-extra" href="https://dwn.robotaset.com/apk/spy77/Spy77.apk" style="margin-right:5px" rel="nofollow" target="_blank">ANTI NAWALA</a></li>
+    <li><a class="btn-custom-extra" href="/demo/slot--pgs/" font-weight:bold;">SLOT DEMO PGSOFT</a></li>
   `;
 
   document.querySelectorAll('main ul')[0].after(ul)
